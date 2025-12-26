6efc22d02d2e75b6f06a49bece5294525c23956b
lukas-sgx <lukas.soigneux@epitech.eu>
2025-12-26 02:15:06 +0100

    [+] Add server handle connection

---
+++ c2/include/server.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** ~/epitech/free-project/Nextrace/c2
+** File description:
+** server
+*/
+
+#ifndef SERVER_H_
+    #define SERVER_H_
+
+int server(int port);
+
+#endif /* SERVER_H_ */
